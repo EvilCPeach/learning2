e1233ec7216adc6ca7e9689673c50aa60e72ba12
student <student@design.local>
2024-12-03 19:10:15 +0200

    сделал стили

--- index.php
+++ index.php
@@ -427,6 +427,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./styles/indexStyle.css">
     <title>Document</title>
     <link rel="stylesheet" href="style.css">
     <!-- <form action="" method="POST"> -->
@@ -643,6 +644,7 @@
 </head>
 <body>
     <form action="" method="POST">
+        <h1>Добро пожаловать!</h1>
         <input type="text" name="login" id="login" placeholder="Введите логин">
         <input type="password" name="password" id="password" placeholder="Введите пароль">
         <input type="submit" id="start">
